e5c7fba30a4a15db123f9697599da372caeaa76b
Ahmad Shah <ah.seerat@gmail.com>
2022-06-28 15:09:05 -0400

    commit tree 14312e97d88b3f6cdd4e9f77a6b5cf53c67f651e

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,28 @@
 # Use this space to code along with the readme
 
-def greeting
+phrase = "Hello World"
+puts phrase 
+puts phrase 
+puts phrase 
+puts phrase 
+puts phrase 
+puts phrase 
+puts phrase 
+puts phrase 
+
+def greeting 
   puts "Hello World"
 end
+
+greeting 
+
+def greeting 
+  phrase = "Hello World"
+  puts phrase 
+  puts phrase 
+  puts phrase 
+  puts phrase 
+end
+
 greeting
+
